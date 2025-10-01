c604e440338dfbfbf61e0889a108fdb3a153e28b
ahmednaggar-ai <a.elnaggar@engaz.ai>
2025-10-01 22:52:05 +0300

    add api-routes

---
+++ src/app/core/constants/api-routes.ts
@@ -0,0 +1,19 @@
+export const ApiRoutes = {
+  // auth: {
+  //   login: '/auth/login',
+  //   logout: '/auth/logout',
+  //   register: '/auth/register',
+  // },
+  // employees: {
+  //   list: '/employees',
+  //   details: (id: number) => `/employees/${id}`,
+  //   create: '/employees',
+  //   update: (id: number) => `/employees/${id}`,
+  //   delete: (id: number) => `/employees/${id}`,
+  // },
+  // payroll: {
+  //   reports: '/payroll/reports',
+  //   earnings: '/payroll/earnings',
+  //   deductions: '/payroll/deductions',
+  // },
+};
